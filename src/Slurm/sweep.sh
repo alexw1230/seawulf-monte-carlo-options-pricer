4c0b371bf2a759bd0f3e58d70b319c1201d2dd6b
alexw1230 <alexanderwiegand310@gmail.com>
2026-09-17 19:18:34 -0400

    sweep sh rev 1

---
+++ src/Slurm/sweep.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=sweep
+#SBATCH -p short-40core
+#SBATCH -N 2
+#SBATCH -n 8
+#SBATCH -t 2:00:00
+#SBATCH -o HPC_Logs/sweep_%j.out
+#SBATCH -e HPC_Logs/sweep_%j.err
+
+module purge
+module load slurm
+module load mpi4py/latest
+cd src
+
+P=$SLURM_NTASKS
+RESULTS=../results_P${P}.csv
+
+N_VALUES=(100000 1000000 10000000 100000000 1000000000 10000000000)
+ 
+for N in "${N_VALUES[@]}"; do
+    echo "$N"
+    mpirun -n $SLURM_NTASKS python3 -m Seawulf.pricer --n "$N" --csv >> "$RESULTS"
+done
